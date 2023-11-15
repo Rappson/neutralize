9f32de04408809d604dec0692a1bd99fe07f4ff7
Axel Grolier <69462172+Rappson@users.noreply.github.com>
2023-11-15 17:22:50 +0100

    mise en place du footer

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -2,3 +2,13 @@
 // fiche contact
 // logo
 // btn de retour en haut de page
+
+function footer(){
+    return (
+        <div className="footer">
+            Mon footer
+        </div>
+    )
+}
+
+export default footer;
