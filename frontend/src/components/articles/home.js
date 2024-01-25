138fc5f37f010131b8b4116ab1f84f3df29f1b25
rappson <axel.grolier@gmail.com>
2024-01-25 10:55:33 +0100

    correction du problème bootstrap

--- frontend/src/components/articles/home.js
+++ frontend/src/components/articles/home.js
@@ -107,11 +107,13 @@ function Home() {
 
             <div className='contact'>
                 <div className="contact-title">
-                    <h2>Contactez nous</h2>
+                    <h2>Contactez nous:</h2>
+                    <h3 className="devis h5">diagnostic et devis gratuit</h3>
                 </div>
 
                 {/* cards de contacts */}
                 <div className='contact-cards'>
+                    
                     <p className='mobile'>Téléphone : <a href='#'>06 12 34 56 78</a></p>
                     <p className='mail'>E-mail : <a href="#">votre.mail@exemple.com</a></p>
                 </div>
